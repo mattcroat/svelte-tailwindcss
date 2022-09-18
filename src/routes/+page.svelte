b9beac09e0711e057a53ed31060b6c9a39f47b82
mattcroat <mattcroat.dev@gmail.com>
2022-09-18 12:42:35 +0200

    feat: Add Prettier plugin for Tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<!-- Before -->
+<button
+	class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"
+>
+	Button
+</button>
+
+<!-- After -->
+<button
+	class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+>
+	Button
+</button>
